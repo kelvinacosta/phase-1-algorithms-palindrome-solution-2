82ea1a9d2a292f1142e8df0c9040a2ed6d0b9703
Kelvin Acosta <acosta.kelvin02@gmail.com>
2023-05-02 23:13:29 -0400

    adds a second solution to check if its a palindrome

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  //Cretae a for loop to iterate the string
+  for(let i =0; i<word.length/2; i++){
+    //create a variable to got in reverse.
+    const j = word.length - 1 -i
+    //create a condition to check if isPalindrome
+    if(word[i]!== word[j])
+      return false
+    
+  }
+  return true
 }
 
 /* 
   Add your pseudocode here
+  iterate the string from one point the half. 
+  Then compare if word isPalindrome return true 
+  otherwise return false
 */
 
 /*
